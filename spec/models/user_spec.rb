8436776b2028fcb544e91c875db0ed81810594d6
Stas <stanislav.belkov@gmail.com>
2013-01-08 12:59:35 +1100

    refactored the authentication tests. Having trouble making the authentication fail.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,56 @@
 require 'spec_helper'
 
 describe User do
-  context 'association/validation' do
-    it { should belong_to(:account) }
+  it { should belong_to(:account) }
 
-    it { should validate_confirmation_of(:password) }
+  it { should validate_confirmation_of(:password) }
 
-    it { should validate_presence_of(:password) }
-    it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:name) }
 
-    it { should validate_uniqueness_of(:email) }
-  end
+  it { should validate_uniqueness_of(:email) }
 
-  FactoryGirl.define do
-    factory :user do
-      name "Stas"
-      email "stas@gmail.com"
-      password "abc123"
+  context 'authentication' do
+
+    it "authenticates with valid credentials" do
+      user = User.create!(:name => 'John', :email => 'john@gmail.com', :password => 'foo')
+      User.authenticate('john@gmail.com', 'foo').should == user
     end
+
+    it "doesn't authenticate with invalid credentials" do
+      user = User.create!(:name => 'John', :email => 'john@gmail.com', :password => 'foo')
+      User.authenticate('john@gmail.com', 'incorrect').should be_nil
+    end
+
+
+    it "generates the password salt"
+
+    it "generates the password hash"
+
   end
+  # FactoryGirl.define do
+  #   factory :user do
+  #     name "Stas"
+  #     email "stas@gmail.com"
+  #     password "abc123"
+  #   end
+  # end
 
-  test_user = FactoryGirl.create(:user, name: "John", email: "Jonno" )
+  # test_user = FactoryGirl.create(:user, name: "John", email: "Jonno" )
 
-  it "authentication succeeds with correct credentials" do
-    User.authentication(test_user.email, test_user.password).should_not == nil
-  end
+  # it "authentication succeeds with correct credentials" do
+  #   User.authentication(test_user.email, test_user.password).should_not == nil
+  # end
 
-  it "authentication fails with incorrect credentials"
+  # it "authentication fails with incorrect credentials"
 
-  it "generates a password salt" do
-    test_user.password_salt.should_not be_empty
-  end
+  # it "generates a password salt" do
+  #   test_user.password_salt.should_not be_empty
+  # end
 
-  it "generates a password hash" do
-    test_user.password_hash.should_not be_empty
-  end
+  # it "generates a password hash" do
+  #   test_user.password_hash.should_not be_empty
+  # end
 
 end
